83fb395f4bb0683bce20d5dd22bcb5ffb692af71
Daniel Tunka <danieltunka42@gmail.com>
2025-08-21 00:57:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className = 'App'>
+      <div className = 'Background-image'> </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
